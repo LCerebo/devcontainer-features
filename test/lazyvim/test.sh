a3e0c035dc615ca3cfec8f3cc1aa6ed387a75ed7
Fulvius <liceocerebotani1@gmail.com>
2026-02-23 22:51:34 +0100

    lazyvim: fixed test for debian images.

--- test/lazyvim/test.sh
+++ test/lazyvim/test.sh
@@ -5,10 +5,13 @@ set -e
 source dev-container-features-test-lib
 
 config_home="${_REMOTE_USER_HOME:-${HOME}}"
+expected_user="${_REMOTE_USER:-$(id -un)}"
 
 check "config directory exists" test -d "${config_home}/.config/nvim"
 check "lazyvim files present" test -f "${config_home}/.config/nvim/lua/config/lazy.lua"
 check "git metadata removed" test ! -d "${config_home}/.config/nvim/.git"
-check "nvim config not owned by root" bash -c "[ \"\$(stat -c '%U' '${config_home}/.config/nvim')\" != 'root' ]"
+if [ "${expected_user}" != "root" ]; then
+	check "nvim config owned by expected user" bash -c "[ \"\$(stat -c '%U' '${config_home}/.config/nvim')\" = '${expected_user}' ]"
+fi
 
 reportResults
