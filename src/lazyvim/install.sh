47948646a709f409c4e2425030b37b52bf6900fe
Fulvius <liceocerebotani1@gmail.com>
2026-02-23 23:52:41 +0100

    lazyvim: Added installation of missing tools and moved apt-get in a single layer for efficency.

--- src/lazyvim/install.sh
+++ src/lazyvim/install.sh
@@ -1,9 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 echo "Installing lazyvim configuration"
 target_user="${_REMOTE_USER:-${USER}}"
 target_home="${_REMOTE_USER_HOME:-${HOME}}"
+required_tools="git curl gcc make"
+
+check_command() {
+  if ! command -v "$1" >/dev/null 2>&1; then
+    echo "$1 is not available;" >&2
+    return 1
+  fi
+  return 0
+}
+
+install_missing_tools() {
+  declare -a missing_tools=()
+  for tool in ${required_tools}; do
+    if ! check_command "${tool}"; then
+      missing_tools+=("${tool}")
+    fi
+  done
+  if [ ${#missing_tools[@]} -gt 0 ]; then
+    if check_command "apt-get"; then
+      echo "Installing missing tools: '${missing_tools[*]}'"
+      apt-get update
+      apt-get install -y --no-install-recommends "${missing_tools[@]}"
+      rm -rf /var/lib/apt/lists/*
+    else
+      echo "apt-get missing, can't install missing tools: '${missing_tools[*]}'." >&2
+      exit 1
+    fi
+  fi
+}
+
+install_missing_tools
 
 git clone https://github.com/LazyVim/starter "${target_home}/.config/nvim"
 rm -rf "${target_home}/.config/nvim/.git"
